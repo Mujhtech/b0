ab172eb8f94aa7d106078c5d0340e1cbb9ebdddd
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2025-01-02 11:16:17 +0100

    feat: implement AI model picker component and update AskB0 to use it

--- frontend/app/components/builder/ask-ai.tsx
+++ frontend/app/components/builder/ask-ai.tsx
@@ -1,14 +1,13 @@
-import { PaperPlaneTilt, Sparkle } from "@phosphor-icons/react";
+import { PaperPlaneTilt } from "@phosphor-icons/react";
 import React from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { AIModelPicker } from "./model-picker";
 
 export default function AskB0() {
   return (
     <div className="h-8 border border-input bg-background shadow-lg flex">
-      <div className="h-8 border-r border-input flex items-center px-2">
-        <Sparkle size={20} className="h-4 w-4" />
-      </div>
+      <AIModelPicker />
       <Input
         className="h-8 border-none focus-visible:ring-0 w-72 px-2"
         placeholder="Ask b0 anything..."
---
+++ frontend/app/components/builder/model-picker.tsx
@@ -0,0 +1,26 @@
+import { Sparkle } from "@phosphor-icons/react";
+import * as React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+} from "~/components/ui/select";
+
+export function AIModelPicker() {
+  return (
+    <Select>
+      <SelectTrigger className="focus:outline-none focus-visible:ring-0 h-8 w-auto  border-r border-t-0 border-l-0 border-b-0">
+        <Sparkle size={20} className="h-4 w-4 mr-1" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="claude-sonnet-3.5">Claude Sonnet 3.5</SelectItem>
+          <SelectItem value="gpt-4o">GPT 4o</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
