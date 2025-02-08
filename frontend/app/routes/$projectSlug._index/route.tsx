71baa4c0dad5746979c480cf15dd21600165ab41
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2025-02-08 21:38:38 +0100

    feat(page): implement SSE for real-time task updates; display loading state during processing

--- frontend/app/routes/$projectSlug._index/route.tsx
+++ frontend/app/routes/$projectSlug._index/route.tsx
@@ -7,8 +7,53 @@ import { PlaygroundProvider } from "~/components/builder/playground/provider";
 import Playground from "~/components/builder/playground/playground";
 import BuilderMenu from "~/components/builder/builder-menu";
 import { PlaygroundBuilderProvider } from "~/components/builder/provider";
+import { useSSE } from "~/hooks/use-ses";
+import { useAuthToken, usePlatformUrl } from "~/hooks/use-user";
+import { useProject } from "~/hooks/use-project";
+import { useCallback, useMemo, useState } from "react";
+import { Spinner } from "@phosphor-icons/react";
 
 export default function Page() {
+  const platformUrl = usePlatformUrl();
+  const project = useProject();
+  const accessToken = useAuthToken();
+  const DEFAULT_EVENTS = [
+    "task_started",
+    "task_updated",
+    "task_completed",
+    "task_failed",
+  ];
+  const [isThinking, setIsThinking] = useState(false);
+
+  useSSE({
+    baseUrl: `${platformUrl}/projects/${project.id}/sse`,
+    shouldRun: true,
+    projectId: project.id,
+    accessToken: accessToken ?? "",
+    events: useMemo(() => DEFAULT_EVENTS, []),
+    onEvent: useCallback((type: string, data: any) => {
+      const { message, workflows, error } = JSON.parse(data);
+
+      console.log("TYPE", type);
+      console.log("MESSAGE", message);
+      if (error != undefined) {
+        console.log("ERROR", error);
+      }
+
+      if (type === "task_started") {
+        setIsThinking(true);
+      }
+
+      if (type === "task_completed" || type === "task_failed") {
+        setIsThinking(false);
+      }
+
+      if (type === "task_updated" && workflows != undefined) {
+        console.log("WORKFLOWS", workflows);
+      }
+    }, []),
+  });
+
   return (
     <PlaygroundBuilderProvider>
       <PlaygroundProvider>
@@ -16,6 +61,17 @@ export default function Page() {
           <Playground />
 
           <div className="absolute bottom-4 w-full z-10">
+            {isThinking && (
+              <div className="flex items-center justify-center gap-2 mb-1">
+                <Spinner
+                  size={18}
+                  className="text-muted-foreground animate-spin"
+                />
+                <p className="font-mono text-[10px] text-muted-foreground text-center">
+                  b0 is thinking...
+                </p>
+              </div>
+            )}
             <div className="flex justify-between">
               <div className=" ml-4">
                 <div className="flex items-center gap-2">
@@ -24,7 +80,7 @@ export default function Page() {
                 </div>
               </div>
               <div className="flex flex-col gap-0.5">
-                <AskB0 />
+                <AskB0 isThinking={isThinking} />
                 <div className="flex items-center justify-center">
                   <p className="font-mono text-[10px] text-muted-foreground text-center">
                     b0 can make mistakes. Please double-check it.
