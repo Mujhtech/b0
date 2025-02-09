433db4c2badce33b4cbe56149ea3a5ea8a57f1da
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2025-02-09 17:37:33 +0100

    feat(database): add methods to find and delete AI token credits and project logs by owner ID and project ID

--- backend/database/store/ai_token_credit.go
+++ backend/database/store/ai_token_credit.go
@@ -1,6 +1,12 @@
 package store
 
-import "github.com/mujhtech/b0/database"
+import (
+	"context"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/mujhtech/b0/database"
+	"github.com/mujhtech/b0/database/models"
+)
 
 const (
 	aiTokenCreditBaseTable    = "ai_token_credits"
@@ -15,4 +21,49 @@ func NewAITokenCreditRepository(db *database.Database) AITokenCreditRepository {
 	return &aiTokenCreditRepo{
 		db: db,
 	}
+}
+
+// FindLogsByProjectID implements AITokenCreditRepository.
+func (a *aiTokenCreditRepo) FindAITokenCreditByOwnerID(ctx context.Context, ownerId string) ([]*models.AITokenCredit, error) {
+	stmt := Builder.
+		Select(aiTokenCreditSelectColumn).
+		From(aiTokenCreditBaseTable).
+		Where(squirrel.Eq{"owner_id": ownerId}).
+		Where(excludeDeleted)
+
+	sql, args, err := stmt.ToSql()
+
+	if err != nil {
+		return nil, err
+	}
+
+	dst := []*models.AITokenCredit{}
+	if err := a.db.GetDB().SelectContext(ctx, &dst, sql, args...); err != nil {
+		return nil, ProcessSQLErrorfWithCtx(ctx, sql, err, "failed to find ai token credits by owner id")
+	}
+
+	return dst, nil
+}
+
+// DeleteAITokenCredit implements AITokenCreditRepository.
+func (a *aiUsageRepo) DeleteAITokenCredit(ctx context.Context, id string) error {
+	stmt := Builder.
+		Update(aiTokenCreditBaseTable).
+		Set("deleted_at", "NOW()").
+		Where(squirrel.Eq{"id": id}).
+		Where(excludeDeleted)
+
+	sql, args, err := stmt.ToSql()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = a.db.GetDB().ExecContext(ctx, sql, args...)
+
+	if err != nil {
+		return ProcessSQLErrorfWithCtx(ctx, sql, err, "failed to delete ai token credit")
+	}
+
+	return nil
 }
