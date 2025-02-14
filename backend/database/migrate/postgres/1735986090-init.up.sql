552c395712a656eb337c5e27c15ab9ac5ac75c50
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2025-02-14 11:34:13 +0100

    feat(database): add initial SQL setup for database creation and user privileges

--- backend/database/migrate/postgres/1735986090-init.up.sql
+++ backend/database/migrate/postgres/1735986090-init.up.sql
@@ -1,5 +1,13 @@
+-- Create DB if not exists
+-- CREATE DATABASE IF NOT EXISTS "b0";
+
+-- Assign postgres user to database
+-- GRANT ALL PRIVILEGES ON DATABASE "b0" TO postgres;
+
+-- Create extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Create table enums
 CREATE TYPE authentication_method AS ENUM ('google', 'github', 'password');
 
 CREATE TYPE token_type AS ENUM ('bearer', 'basic', 'api_key');
